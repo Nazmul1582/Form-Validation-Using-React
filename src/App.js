8b93492dcb89214205c93e451a2b47691a50929c
Nazmul1582 <nazmul1582>
2022-06-01 20:22:54 +0600

    Now I've used react hook to interactive the form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+  const handleChange = (event) => {
+    const {name, value} = event.target;
+    setValues({...values, [name] : value})
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(values)
+  }
+
   return (
     <div className="wrapper">
       <div className="form">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h2 className="title">Register Form</h2>
           <div className="field">
             <label>Username</label>
             <input 
               type="text" 
               name="username"
+              value={values.username} 
               placeholder="Username"
+              onChange={handleChange} 
             />
           </div>
           <div className="field">
@@ -19,7 +38,9 @@ function App() {
             <input 
               type="email" 
               name="email"
+              value={values.email} 
               placeholder="Email"
+              onChange={handleChange} 
             />
           </div>
           <div className="field">
@@ -27,7 +48,9 @@ function App() {
             <input 
               type="password" 
               name="password"
+              value={values.password} 
               placeholder="Password"
+              onChange={handleChange} 
             />
           </div>
           <div className="field">
@@ -35,7 +58,9 @@ function App() {
             <input 
               type="password" 
               name="confirmPassword"
+              value={values.confirmPassword} 
               placeholder="Confirm Password"
+              onChange={handleChange} 
             />
           </div>
           <button className='btn'>Submit</button>
