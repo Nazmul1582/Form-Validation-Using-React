4e6538c4c9b4261bd2a9e1de10c76ea68393edd2
Nazmul1582 <nazmul1582>
2022-06-01 20:32:33 +0600

    Finally I've validate this register from

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
     password: "",
     confirmPassword: ""
   });
+  const [errors, setErrors] = useState("");
+  const [isSubmit, setIsSubmit] = useState(false)
 
   const handleChange = (event) => {
     const {name, value} = event.target;
@@ -15,13 +18,54 @@ function App() {
   }
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(values)
+    setErrors(validate(values))
+    setIsSubmit(true)
+  }
+
+  useEffect(() => {
+    if(Object.keys(errors).length === 0 && isSubmit ){
+      console.log("Informations = ", values)
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [errors])
+
+  const validate = (input) => {
+    const errorMsg = {}
+    const regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    if(input.username === ""){
+      errorMsg.username = "Please enter your username"
+    }
+
+    if(input.email === ""){
+      errorMsg.email = "Please enter your email"
+    }else if(!regex.test(input.email)){
+      errorMsg.email = "This is not a valid email format!"
+    }
+
+    if(input.password === ""){
+      errorMsg.password = "Please enter password"
+    }else if(input.password.length <= 5){
+      errorMsg.password = "Password must be more than 5 characters!"
+    }else if(input.password.length >= 12){
+      errorMsg.password = "Password can't be more than 12 characters!"
+    }
+
+    if(input.confirmPassword === ""){
+      errorMsg.confirmPassword = "Please enter confirm password"
+    }else if(input.confirmPassword !== input.password){
+      errorMsg.confirmPassword = "Your password hasn't matched!"
+    }
+
+    return errorMsg;
   }
 
   return (
     <div className="wrapper">
       <div className="form">
-        <form onSubmit={handleSubmit}>
+        {Object.keys(errors).length === 0 && isSubmit ? 
+        (<p className="successfulMsg">Submitted successfully!</p>) 
+        : 
+        (<form onSubmit={handleSubmit}>
           <h2 className="title">Register Form</h2>
           <div className="field">
             <label>Username</label>
@@ -33,6 +77,7 @@ function App() {
               onChange={handleChange} 
             />
           </div>
+          <p className="errorMsg">{errors.username}</p>
           <div className="field">
             <label>Email</label>
             <input 
@@ -43,6 +88,7 @@ function App() {
               onChange={handleChange} 
             />
           </div>
+          <p className="errorMsg">{errors.email}</p>
           <div className="field">
             <label>Password</label>
             <input 
@@ -53,6 +99,7 @@ function App() {
               onChange={handleChange} 
             />
           </div>
+          <p className="errorMsg">{errors.password}</p>
           <div className="field">
             <label>Confirm password</label>
             <input 
@@ -63,8 +110,10 @@ function App() {
               onChange={handleChange} 
             />
           </div>
+          <p className="errorMsg">{errors.confirmPassword}</p>
           <button className='btn'>Submit</button>
         </form>
+        )}
       </div>
     </div>
   );
